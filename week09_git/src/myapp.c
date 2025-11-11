2b84055fc924fe4b4b49fa466202ef84e439eab4
Minseo Kim <202401647@inu.ac.kr>
2025-11-11 11:02:42 +0900

    add my basic ops

--- week09_git/src/myapp.c
+++ week09_git/src/myapp.c
@@ -2,6 +2,23 @@
 
 double calculate(char* op, double a, double b){
 	printf("Calculating.....\n");
+
+	if(strcmp(op, "+") == 0){
+		return myadd(a, b);
+	}
+	else if(strcmp(op, "-") == 0){
+		return mysub(a, b);
+	}
+	else if(strcmp(op, "*") == 0){
+		return mymul(a, b);
+	}
+	else if(strcmp(op, "/") == 0){
+		return mydiv(a, b);
+	}
+	else{
+		printf("Unknown operator: %s\n", op);
+		return 0;
+	}
 }
 
 int main(void){
@@ -9,7 +26,7 @@ int main(void){
 	char op[3];
 
 	printf("~~~ Welcome to my calculator ~~~\n\n");
-	printf("Enter expression (e.g. 1 +2): ");
+	printf("Enter expression (e.g. 1 + 2 ): ");
 	scanf("%lf %s %lf", &a, op, &b);
 	printf("Result = %.2lf\n", calculate(op, a, b));
 	
